e2818bde954d6e8c9e81131042c2009898829aec
unknown <aelise17264@gmail.com>
2020-09-03 14:19:28 -0600

    tested input fields

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -40,15 +40,17 @@ const ContactForm = () => {
         <div>
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
-          </label>
+          
           <input name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
+          </label>
         </div>
         <div>
-          <label htmlFor="message">Message</label>
+          <label htmlFor="message">Message
           <textarea name="message" ref={register({ required: false })} />
+        </label>
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
